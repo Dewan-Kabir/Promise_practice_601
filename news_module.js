4e63d284fc395efdd350f274103456b999f7a5ef
dkabir94@bu.edu <Dkabir94@bu.edu>
2021-04-19 07:41:51 -0500

    working on json object

--- news_module.js
+++ news_module.js
@@ -1,43 +1,49 @@
-const news = [
-
-	{
-
-		caption: "Trump adopted by the Queen of Yorkshire",
-
-		source: "Fox News",
-
-		url: "http://www.foxnews.com",
-
-		author: "Joe Mann",
-
-		released: "2020-01-17 12:01:28 AM EST",
-
-		story: "This is a really interesting story.  More to follow.."
-
-	},
-
-	{
-
-		caption: "Dog for hire.  Solves crime and fluffy",
-
-		source: "The Voxx",
-
-		url: "https://www.washingtonpost.com/",
-
-		author: "Mary Blige",
-
-		released: "2019-11-17 8:21:01 PM EST",
-
-		story: "Makes make hope that a dog can become President.  Hell, Trump can do it..."
-
-	}
-
-];
-
-var news_content_html = document.getElementById('news_content');
-var content_table = document.getElementById('my_table');
-
 export function findByAuthor(){
+
+
+
+    const news = [
+
+        {
+    
+            caption: "Trump adopted by the Queen of Yorkshire",
+    
+            source: "Fox News",
+    
+            url: "http://www.foxnews.com",
+    
+            author: "Joe Mann",
+    
+            released: "2020-01-17 12:01:28 AM EST",
+    
+            story: "This is a really interesting story.  More to follow.."
+    
+        },
+    
+        {
+    
+            caption: "Dog for hire.  Solves crime and fluffy",
+    
+            source: "The Voxx",
+    
+            url: "https://www.washingtonpost.com/",
+    
+            author: "Mary Blige",
+    
+            released: "2019-11-17 8:21:01 PM EST",
+    
+            story: "Makes make hope that a dog can become President.  Hell, Trump can do it..."
+    
+        }
+    
+    ];
+
+
+
+
+
+    var news_content_html = document.getElementById('news_content');
+var content_table = document.getElementById('my_table');
 
     targetAuthor = toString(document.getElementById('author_input')).value;
 

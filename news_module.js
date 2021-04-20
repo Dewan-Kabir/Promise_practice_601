58f8dda18e6687e08f2781492edcd6ba8faf8655
dkabir94@bu.edu <Dkabir94@bu.edu>
2021-04-20 05:16:33 -0500

    working with date

--- news_module.js
+++ news_module.js
@@ -56,7 +56,7 @@ console.log(resp_json_par[0].author);
 
 console.log(resp_json_par[0].keyword[1]);
 
-console.log(str.substr(resp_json_par[0].released));
+console.log(substr(resp_json_par[0].released));
 
 
 /** 
@@ -173,5 +173,76 @@ for (var k = 0 ; k < resp_json_par.length; k ++){
 
     findByKeyword();
 
+/***************************************************************************************** */
 
+
+export function findBydate(){
+
+
+
+    
+
+    var resp_json = JSON.stringify(news);
+    
+    console.log(resp_json[0]);
+    
+    var resp_json_par = JSON.parse(resp_json);
+    
+    console.log(resp_json_par);
+    
+    console.log(resp_json_par[0].author);
+    
+    console.log(resp_json_par[0].keyword[1]);
+    
+    
+    /** 
+    console.log(resp_json[0]);
+    
+    **/
+    
+    var news_content_html = document.getElementById('news_content');
+    var content_table = document.getElementById('my_table');
+    var targetAuthor = (document.getElementById('author_input').value).toString();
+    var targetKeyword = (document.getElementById('keyword_input').value).toString();
+    console.log(targetKeyword);
+    
+    
+        
+    
+    let results =[];
+    
+        if (targetKeyword === null){
+            return results;
+            
+        }
+    
+        console.log(results);
+    
+
+        
+        
+
+        
+for (var k = 0 ; k < resp_json_par.length; k ++){
+            var test_news = resp_json_par[k];
+        for ( var i = 0 ; i < test_news.keyword.length ; i ++){
+             var match_keyword = test_news.keyword[i];
+
+        /**  for (var j = 0 ; j< match_keyword.length ; j ++){ **/ /** Need to add the ending } */
+                if (targetKeyword === match_keyword){
+                    results.push(targetKeyword);
+                    console.log(results);
+                    news_content_html.innerHTML = test_news.story;
+
+                }else{
+                    alert('There is no news for that author');
+                }
+            
+        }
+    }
+
+        return results;
+    }
+
+    findByKeyword();
 

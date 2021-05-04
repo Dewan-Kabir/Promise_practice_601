aa919d52f5047d955f87837b0a6d13b043f2dc4a
dkabir94@bu.edu <Dkabir94@bu.edu>
2021-05-04 16:40:28 -0500

    onload html

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 import {findByAuthor, findByKeyword , findBydate ,myPageLoad} from './news_module.js';
 
-document.getElementsByTagName('body').addEventListener('load' , myPageLoad);
+
 
 document.getElementById('button_get_news').addEventListener('click', findByAuthor);
 
